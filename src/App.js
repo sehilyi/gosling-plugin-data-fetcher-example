be1a65d9eab731a0c19de8fa2c7cbcdda282f7bd
Sehi L'Yi <sehilyi@gmail.com>
2022-07-07 13:36:31 -0400

    chore: first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { GoslingComponent } from 'gosling.js';
+import CistromeDataFetcher from "./plugin/cistrome-datafetcher";
+import { default as hgRegister } from "higlass-register";
+
+// register the custom data-fetcher
+hgRegister(
+    { dataFetcher: CistromeDataFetcher, config: CistromeDataFetcher.config },
+    { pluginType: "dataFetcher" }
+);
+
+function App() {
+	return (
+		<GoslingComponent spec={{
+			tracks: [{
+				data: {
+					type: 'experimentalPlugin',
+					name: 'cistrome',
+					options: {
+						cid: 1,
+						chromSizesUrl: "https://aveit.s3.amazonaws.com/higlass/data/sequence/hg38.chrom.sizes",
+					}
+				},
+				title: 'Gosling Track Based On Plugin Data-Fetcher',
+				mark: "bar",
+				x: { field: "start", type: "genomic" },
+				xe: { field: "end", type: "genomic" },
+				y: { field: "value", type: "quantitative", axis: "none" },
+				color: { value: "#22908D" },
+				tooltip: [
+					{ field: "start", type: "genomic" },
+					{ field: "end", type: "genomic" },
+					{ field: "value", type: "quantitative" },
+				],
+				style: { outlineWidth: 0 },
+				width: 600,
+				height: 100
+			}]
+		}}/>
+	);
+}
+
+export default App;
